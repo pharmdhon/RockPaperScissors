9afc279ddecea02a7a20fd48a68c9a85478fbcb7
pharmdhon <62495057+pharmdhon@users.noreply.github.com>
2022-03-23 17:29:21 -0700

    Most function is complete, now will just need to add features that prevent unexpected input and also clean up code and possibly something at the end that declares the winner of the 5 round game

--- javascript.js
+++ javascript.js
@@ -1,39 +1,159 @@
-/* Startng over, step 1 initialize variables needed*/
-let computerSelection;
-let playerSelection;
-let computerScore = 0;
-let playerScore = 0;
+// Possible Choices
+choices = ['ROCK','PAPER','SCISSORS'];
 
-// Initialize Computer Selection to a random choice from an array
-choices = ["ROCK","PAPER","SCISSORS"];
-computerSelection = choices[Math.floor(Math.random()*choices.length)];
+function game(){
+    for(let i = 0; i<5; i++){
+        playRound();
+    }
+}
+
+game();
+//}
+
+function playRound(){
+  const playerCf = playerChoice();
+  const computerCf = computerChoice();
+  checkWinner(computerCf,playerCf);
+  console.log(playerCf);
+  console.log(computerCf);
+  console.log(checkWinner(computerCf,playerCf));
+}
+
+// create a function that obtains the choice from the player the creates a variable based on the input and then assigns the value of the function to a variable inside playround
+
+function playerChoice () {
+    let input = prompt('Lets play ROCK, PAPER, SCISSORS, ready..... shoot');
+    return input;
+}
+    //while(input == null){
+ //       input = prompt (' Please only choose ROCK, PAPER, or SCISSORS');
+ //   }
+  //  input = input.toUpperCase(); 
+  //  let check = validateInput(input)
+  //  while(check == false){
+ //      input = prompt('Please only choose ROCK, PAPER, or SCISSORS');
+ //      input = input.toUpperCase();
+ //      check = validateInput(input);
+ //   }
+    
+   // console.log(input); 
+//}
+let playerC = playerChoice();
+// create a function that randomly generates a cpu choice, take the result of the function and create a constant inside the playRound function
+
+function computerChoice() {
+    return choices[Math.floor(Math.random()*choices.length)];
+}
+
+let computerC = computerChoice();
+//function validateInput(choice){
+   // if(choices.includes(choice)){
+ //       return true;
+//       }else {
+//           return false;
+//       }
+//}
+// Remember 1 function for 1 task...dont try to jumble up the ability to check winners in playRound lets create a seperate function to check who the winner is 
+function  checkWinner (choiceC, choiceP){
+    if (choiceC === choiceP){
+        return 'Tie';
+    }else if(
+        (choiceC === 'ROCK' && choiceP === 'SCISSORS')||
+        (choiceC === 'PAPER' && choiceP === 'ROCK') ||
+        (choiceC === 'SCISSORS' && choiceP === 'PAPER')
+    ) {
+        return 'computer';
+    }else if(
+        (choiceC === 'ROCK' && choiceP === 'PAPER')||
+        (choiceC === 'PAPER' && choiceP === 'SCISSORS') ||
+        (choiceC === 'SCISSORS' && choiceP === 'ROCK')
+    ) {
+        return 'player';
+     }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let playerScore =0;
+let computerScore = 0;
+let roundWinner = "";
+
+function getRandomChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    switch(randomNumber){
+        case 0:
+            return "ROCK"
+        case 1:
+            return "PAPER"
+        case 2:
+            return "SCISSORS"
+    }
+}
+
+const computerSelection = getRandomChoice()
 console.log(computerSelection);
 
-//Initialize a player Selection by getting input from the user, also it is capitalized to make comparison to CPU less complex.
-
-getInput = prompt("Lets Play")
+let getInput = prompt(" Rock, Paper, Scissors, Shoooot");
 playerSelection = getInput.toUpperCase();
 console.log(playerSelection);
 
-//Create a function playGame that takes the computer selection and player slection as inputs compares them and returns a statement about if the
-// was a win, loss or tie, in addition the function keeps score
-
 function playRound(){
-    if (computerSelection === playerSelection) alert('Tie');
-    else if(
+    if (computerSelection === playerSelection){
+        roundWinner = 'Tie'
+    }else if(
         (computerSelection === 'ROCK' && playerSelection === 'SCISSORS')||
         (computerSelection === 'PAPER' && playerSelection === 'ROCK') ||
         (computerSelection === 'SCISSORS' && playerSelection === 'PAPER')
-    ) computerScore = ++computerScore;
-     if (computerScore === 1) alert ('loser');
-    else if(
+    ) {
+        computerScore++
+        roundWinner = 'computer'
+    }else if(
         (computerSelection === 'ROCK' && playerSelection === 'PAPER')||
         (computerSelection === 'PAPER' && playerSelection === 'SCISSORS') ||
         (computerSelection === 'SCISSORS' && playerSelection === 'ROCK')
-    ) playerScore = ++playerScore;
-      if (playerScore === 1) alert ('winner');
+    ) {
+        playerScore++
+        roundWinner = 'player'
+        
+     }
 }
+   
+   playRound();
+   console.log(roundWinner);
+   console.log(computerScore, playerScore);
+   
 
-playRound();
-console.log(computerScore);
-console.log(playerScore);
+
+*/
